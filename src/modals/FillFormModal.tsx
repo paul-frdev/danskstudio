bc9664a858268d9e31124ed4109f84bca0448347
paul-frdev <pavelzadyrko@gmail.com>
2022-05-29 11:53:07 +0300

    fix: styles fillformmodal

--- src/modals/FillFormModal.tsx
+++ src/modals/FillFormModal.tsx
@@ -12,7 +12,7 @@ interface FillFormModalProps {
   toggleShow: () => void;
 }
 
-export const FillFormModal: React.FC<FillFormModalProps> = ({toggleShow, show }) => {
+export const FillFormModal: React.FC<FillFormModalProps> = ({ toggleShow, show }) => {
 
 
   return (
@@ -23,16 +23,33 @@ export const FillFormModal: React.FC<FillFormModalProps> = ({toggleShow, show })
         onClose={toggleShow}
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
+        PaperProps={{
+          style: {
+            backgroundColor: '#fff',
+            maxWidth: '660px',
+            color: 'black',
+            filter: "drop-shadow(2px 6px 10px rgba(0, 0, 0, 0.25))",
+            borderRadius: '15px',
+            border: '1px solid #c90c31'
+          },
+        }}
       >
         <DialogActions>
           <CloseButton onClose={toggleShow} />
         </DialogActions>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description"  className='modals fill-form-modal-text'>
+        <DialogContent
+          style={{ maxWidth: '385px', textAlign: 'center' }}>
+          <DialogContentText
+            id="alert-dialog-description"
+            className='modals fill-form-modal-text'
+            style={{paddingTop: '15px', fontWeight: 700, fontSize: '16px', lineHeight: '150%', color: '#000' }}
+          >
             Заполните нашу анкету на определение уровня владения языком и мы поможем подобрать вам обучающий курс
           </DialogContentText>
         </DialogContent>
-        <DialogActions>
+        <DialogActions
+          style={{ justifyContent: 'center', marginBottom: '20px' }}
+        >
           <NavLink className='fill-form-modal-link' to='/questionary' onClick={toggleShow}>
             заполнить анкету
           </NavLink>
